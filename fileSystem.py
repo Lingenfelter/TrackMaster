8db6534b389ce077c39443fb06331fa16fe900f0
Matthew Lingenfelter <M.Lingenfelter92@gmail.com>
2021-06-27 18:13:28 -0400

    Simple track splitting based on volume

--- fileSystem.py
+++ fileSystem.py
@@ -2,6 +2,9 @@ import os
 from PIL import Image
 import soundfile as sf
 
+saveDir = 'recordings'
+track_num = 0
+
 
 class File:
     def __init__(self, name, filetype, data=None):
@@ -12,7 +15,7 @@ class File:
 
     def get_path(self):
         if self.parent is None:
-            return os.path.join('recordings', self.name)
+            return os.path.join(saveDir, self.name)
         return os.path.join(self.parent.get_path(), self.name)
 
 
@@ -43,20 +46,56 @@ class Directory(File):
 
 class AudioFile(File):
     def __init__(self, name, filetype='.wav'):
-        File.__init__(self, name, filetype)
+        File.__init__(self, name, filetype, data=[0, 0])
+        self.audio_source = 'recordings/working/audio.wav'
+        self.cutoff = 0.0  # Files below this length in seconds are not saved
+        self.padding = 0.0  # Seconds added before and after a track
         self.artist = ''
         self.album = ''
 
+    def set_track(self, start, end):
+        self.data[0] = start
+        self.data[1] = end
+
     def merge(self, target):
         self.data[1] = target.data[1]
-        self.parent.remove_element(target)
+        # self.parent.remove_element(target)
 
     def save(self):
-        with sf.SoundFile('recordings/working/audio.flac') as source:
-            source.seek(self.data[0])
-            path = self.get_path() + self.filetype
-            frames = (self.data[1] - self.data[0])
-            sf.write(path, source.read(frames=frames), source.samplerate)
+        # All print statements are for real-time use debugging, will be used to monitor when connected
+        # to a record player.
+        global track_num
+        with sf.SoundFile(self.audio_source) as source:
+            # Get the beginning of the track
+            start = int(self.data[0])
+            padding = int(self.padding * source.samplerate)
+            # Get the running time of the track
+            frames = (int(self.data[1]) - start)
+            try:
+                # If the running time is longer than the cutoff length, save the track
+                if frames > self.cutoff * source.samplerate:
+                    # Save the track in a "01 - name.file" convention.
+                    track_num += 1
+                    self.name = str('{:02d}'.format(track_num)) + ' - ' + self.name
+
+                    print(f'track {self.name} \t start: {start} \t end: {self.data[1]} \t diff {frames} ')
+
+                    # Add a small amount of recording padding to ensure that the whole note that triggered
+                    # the recording is captured.
+                    start = start - padding if start - padding > 0 else 0
+                    frames += (padding * 2)
+
+                    source.seek(start)
+                    path = self.get_path() + self.filetype
+                    sf.write(path, source.read(frames=frames), source.samplerate)
+                    print(f'{self.name = }: saved')
+                else:
+                    print(f'{self.name = }: did not save due to insufficient length')
+                    print(f'{frames = }\t{self.cutoff * source.samplerate = }')
+            except Exception:
+                print(f'{Exception = }')
+                print('-' * 50)
+                print(f'track {self.name} \t start: {start} \t end: {self.data[1]} \t diff {frames} ')
 
 
 class ImageFile(File):
@@ -72,3 +111,40 @@ class ImageFile(File):
             os.remove(path)
         self.data.save(path)
         self.data.close()
+
+
+class RecordFileSystem:
+    def __init__(self):
+        self.artist = Directory('artist')
+        self.album = Directory('album')
+        self.samplerate = 0
+
+        self.artist.add_child(self.album)
+
+        # How close, in seconds, that the end of one track and the beginning of the next need to be to merge
+        self.merge_threshold = 0.73
+
+    def add_track(self, track):
+
+        # If track is not the first element, compare it to the previous track
+        if len(self.album.data) > 0:
+            track_spacing = self.samplerate * self.merge_threshold
+            prev_track = self.album.data[-1]
+
+            # If this track and the previous track are with-in the merging threshold, merge
+            if track.data[0] - prev_track.data[1] < track_spacing:
+                print(f'{track.name = }\t Merged with {prev_track.name = }')
+                prev_track.merge(track)
+
+            # Otherwise make it a new track.
+            else:
+                print(f'{track.name = }\tGap too large, did not merge -'
+                      f' {track.data[0] - prev_track.data[1]} < {track_spacing}')
+                self.album.add_child(track)
+        else:
+            print(f'{track.name = }\t First element')
+            self.album.add_child(track)
+
+    def save_all(self):
+        print('-' * 50, '\nSaving ...')
+        self.artist.save_tree()
--- gui.py
+++ gui.py
@@ -1,8 +1,13 @@
 from dearpygui.core import *
 from dearpygui.simple import *
 import audioRecorder as aRec
+from fileSystem import RecordFileSystem
 
 currently_recording = False
+x_range = [x for x in range(aRec.plotRecSize)]
+
+# Filesystem head
+file_head: RecordFileSystem
 
 
 def toggle_recording():
@@ -26,21 +31,23 @@ def get_plot_data():
         data = aRec.get_plotRec()
         set_value('ch_1', data[0])
         set_value('ch_2', data[1])
-        x_range = [x for x in range(len(data[0]))]
         add_line_series('audio_plot', 'right channel', x_range, data[0], weight=2.5)
-        add_line_series('audio_plot', 'left channel', x_range, data[1], weight=2.5, color=[155,50,50,75])
+        add_line_series('audio_plot', 'left channel', x_range, data[1], weight=2.5, color=[155, 50, 50, 75])
+        bounds = aRec.recording_threshold
+        add_hline_series('audio_plot', 'bounds', [bounds, -bounds])
 
 
 def show_gui():
     with window('main'):
         with child('l_column', autosize_y=True):
             add_plot('audio_plot', label='', height=300, no_mouse_pos=True,
-                     xaxis_no_tick_labels=True, yaxis_no_tick_labels=True, xaxis_no_tick_marks=True, yaxis_no_tick_marks=True)
+                     xaxis_no_tick_labels=True, yaxis_no_tick_labels=True, xaxis_no_tick_marks=True,
+                     yaxis_no_tick_marks=True)
             set_plot_ylimits('audio_plot', -1, 1)
 
             add_button('rec/stop', label='Record', callback=toggle_recording, width=150, height=50)
             add_same_line()
-            add_slider_float('volume', min_value=0, max_value=1, default_value=0.0,
+            add_slider_float('volume', min_value=0, max_value=1, default_value=aRec.playbackVolume,
                              callback=send_volume, width=200)
 
         add_same_line()
@@ -56,8 +63,8 @@ def show_gui():
         if currently_recording:
             aRec.stop_recording()
 
-    #show_metrics()
-    #show_documentation()
+    # show_metrics()
+    # show_documentation()
 
     set_render_callback(get_plot_data)
     set_start_callback(resize)
